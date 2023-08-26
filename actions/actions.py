a24c4b0d0a5126ed8dac6263b3d571de14d96ac7
devanshi gupta <127221653+Devu0503@users.noreply.github.com>
2023-08-26 18:56:57 +0530

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -6,75 +6,117 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import pymongo
+client=pymongo.MongoClient("mongodb://localhost:27017")
+DB=client["Canteen"]
+TRajColl=DB["Yamuna"]
+class ActionHelloWorld(Action):
 
-# from typing import Any, Text, Dict, List
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        breakFast = TRajColl.find({"_id":1})
+        c=1
+        for i in breakFast:
+            item = i["item"+str(c)]
+            dispatcher.utter_message(item)
+            c=c+1
+            
+#from typing import Any, Text, Dict, List
+#from rasa_sdk import Action, Tracker
+#from rasa_sdk.events import SlotSet
+#from rasa_sdk.executor import CollectingDispatcher
+
+#class ActionSetChosenCanteen(Action):
+#    def name(self) -> Text:
+#        return "action_set_chosen_canteen"
+
+#    def run(self, dispatcher: CollectingDispatcher,
+#            tracker: Tracker,
+#            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#        chosen_canteen = next(tracker.get_latest_entity_values("canteen_name"), None)
+        
+#        if chosen_canteen:
+#            dispatcher.utter_message(text="Great choice! You've chosen the {} Canteen for your order.".format(chosen_canteen))
+#            return [SlotSet("chosen_canteen", chosen_canteen)]
+#        else:
+#            dispatcher.utter_message(text="I'm sorry, I didn't catch which canteen you chose.")
+#            return []
+
+#from typing import Any, Text, Dict, List
+#from rasa_sdk import Action, Tracker
+#from rasa_sdk.executor import CollectingDispatcher
+#import pymongo
+
+#class ActionShowMenu(Action):
+#    def name(self) -> Text:
+#        return "action_show_menu"
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+#    def run(self, dispatcher: CollectingDispatcher,
+#            tracker: Tracker,
+#            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#        # Connect to MongoDB
+#        client = pymongo.MongoClient("mongodb://localhost:27017/")
+#        db = client["your_database_name"]
+#        
+#        # Get the chosen canteen from the slot
+#        chosen_canteen = tracker.get_slot("chosen_canteen")
+#        
+#       # Fetch the menu from the database
+#       menu_collection = db["menu_collection_name"]
+#        menu_data = menu_collection.find_one({"canteen": chosen_canteen})
+#        
+#        if menu_data:
+#            menu_items = menu_data.get("menu_items", [])
+#            if menu_items:
+#                menu_response = "\n".join(menu_items)
+#                dispatcher.utter_message(text=f"Here's the menu for {chosen_canteen}:\n{menu_response}")
+#            else:
+#                dispatcher.utter_message(text=f"Apologies, but it seems like the menu for {chosen_canteen} is not available.")
+#        else:
+#            dispatcher.utter_message(text="I'm sorry, I couldn't find the menu for the selected canteen.")
+#        
+#        return []
 
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
-from rasa_sdk.events import SlotSet, EventType
 from rasa_sdk.executor import CollectingDispatcher
-import webbrowser
+import pymongo
 
-class ActionVideo(Action):
+class ActionShowMenu(Action):
     def name(self) -> Text:
-        return "action_video"
+        return "action_show_menu"
 
-    async def run(
-        self,
-        dispatcher,
-        tracker: Tracker,
-        domain: "DomainDict",
-    ) -> List[Dict[Text, Any]]:
-        video_url="https://youtu.be/jj4BL9o3Q7o"
-        dispatcher.utter_message("wait... Playing your video.")
-        webbrowser.open(video_url)
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Connect to MongoDB
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        db = client["canteen_db"]  # Change to your database name
+        
+        # Get the chosen canteen from the slot
+        chosen_canteen = tracker.get_slot("chosen_canteen")
+        
+        # Fetch the menu from the database
+        menu_collection = db["north canteen"]  # Change to your collection name
+        menu_data = menu_collection.find_one({"canteen": chosen_canteen})
+        
+        if menu_data:
+            menu_items = menu_data.get("menu_items", [])
+            if menu_items:
+                menu_response = "\n".join([f"{item['item']} - ${item['price']:.2f}" for item in menu_items])
+                dispatcher.utter_message(text=f"Here's the menu for {chosen_canteen}:\n{menu_response}")
+        else:
+                dispatcher.utter_message(text=f"Apologies, but it seems like the menu for {chosen_canteen} is not available.")
+         else:
+                dispatcher.utter_message(text="I'm sorry, I couldn't find the menu for the selected canteen.")
+        
         return []
 
-class ValidateRestaurantForm(Action):
-    def name(self) -> Text:
-        return "user_details_form"
 
-    def run(
-        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
-    ) -> List[EventType]:
-        required_slots = ["name", "number"]
-
-        for slot_name in required_slots:
-            if tracker.slots.get(slot_name) is None:
-                # The slot is not filled yet. Request the user to fill this slot next.
-                return [SlotSet("requested_slot", slot_name)]
-
-        # All slots are filled.
-        return [SlotSet("requested_slot", None)]
-
-class ActionSubmit(Action):
-    def name(self) -> Text:
-        return "action_submit"
-
-    def run(
-        self,
-        dispatcher,
-        tracker: Tracker,
-        domain: "DomainDict",
-    ) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(template="utter_details_thanks",
-                                 Name=tracker.get_slot("name"),
-                                 Mobile_number=tracker.get_slot("number"))
